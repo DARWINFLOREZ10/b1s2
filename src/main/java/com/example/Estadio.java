15bb5db99a0bd83df5d3680de8b3991048262eeb
Darwin Florez <darwinflorezcardenas@g,ail.com>
2025-07-15 10:25:34 -0500

    semana2

---
+++ src/main/java/com/example/Estadio.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Estadio {
+     public String nombre;
+    public String ciudad;
+    public int capacidad;
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Capacidad: " + capacidad);
+    }
+}
